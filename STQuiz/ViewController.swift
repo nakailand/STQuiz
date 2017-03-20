4fd87d96242f8fbf30584fcbaa1654e05eba05aa
nakailand <s.nakajima0523@gmail.com>
2017-03-20 17:14:33 +0900

    Initial Commit

---
+++ STQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  STQuiz
+//
+//  Created by nakailand on 2017/03/20.
+//  Copyright © 2017年 nakailand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
